132ed7b8275197ab1adf143e1a664c3a66aac4e6
Maximillian Kosmicki <kosmicki.max@gmail.com>
2018-06-24 11:45:14 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL ##placeholder for inverse
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) minv <<- inv
+  getinv <- function() minv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  if(is.null(inv)) {
+    inv <- solve(x$get())
+    x <- x$setinv(inv)
+  } else {
+    return(inv)
+  }
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
